6938741ca35e7b5b9936cd4bc6176e028b8bbc77
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-11-28 17:15:00 +0530

    students component(props)

---
+++ reactapp-nagarjun/src/Students.js
@@ -0,0 +1,13 @@
+const Students =(props) =>
+{
+    return (
+    <>
+    <div div className = "student">
+    <p> Name : {props.name}</p>
+    <p> Age : {props.age}</p>
+    </div>
+    </>
+    
+    );
+}
+export default Students;
