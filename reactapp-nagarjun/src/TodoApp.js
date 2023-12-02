8720c47f5d41f09ff401dbe03be311a02003b0b1
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-12-02 14:36:29 +0530

    Create TodoApp.js

---
+++ reactapp-nagarjun/src/TodoApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function TodoApp() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim()) {
+      setTasks([...tasks, { id: tasks.length + 1, text: newTask }]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Simple Todo App</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.text}</li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="New task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoApp;
