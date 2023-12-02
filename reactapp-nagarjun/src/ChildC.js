d74adc2fd00c3db8ce4c4aef1219932f01c1d2f2
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-12-02 12:55:12 +0530

    Update ChildC.js

--- reactapp-nagarjun/src/ChildC.js
+++ reactapp-nagarjun/src/ChildC.js
@@ -1,14 +1,14 @@
  import React, {useContext} from 'react'
- import {data,data1} from './App'
+//  import {data,data1} from './App'
  
  
  export default function ChildC() {
-    const name = useContext (data);
-    const gender = useContext(data1);
+    // const name = useContext (data);
+    // const gender = useContext(data1);
 
    return (
      <div>
-       <h1> My name is {name} my gender is {gender}</h1>
+       {/* <h1> My name is {name} my gender is {gender}</h1> */}
      </div>
    )
  }
