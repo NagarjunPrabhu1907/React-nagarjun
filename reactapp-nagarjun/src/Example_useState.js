8e7fe2b3199bf1d2e4147539f1a817af24854ad6
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-12-04 11:32:50 +0530

    useState example1

--- reactapp-nagarjun/src/Example_useState.js
+++ reactapp-nagarjun/src/Example_useState.js
@@ -4,9 +4,13 @@ import React from 'react'
 
 
 const Example_useState = () => {
-  const [isOn, setIsOn] = useState(true);
+const [isOn, setIsOn] = useState(true);
+
+const [input,setInput] =  useState(" ");
+const[words,setWords] = useState([]);
 
   return (
+    <>
     <div>
       <p>Toggle State: {isOn ? 'ON' : 'OFF'}</p>
       <button onClick={() => setIsOn(!isOn)}>Toggle</button>
@@ -14,6 +18,23 @@ const Example_useState = () => {
 
 
 
+    <div className="APP">
+    <h1> Input value : {input}</h1>
+    <input
+    type="text"
+    placeholder="type here"
+    value={input}
+    onChange={(e)=>setInput(e.target.value)} />
+    </div>
+    
+    <button onClick={() => setWords((state) => [...state, input])}>Add Word</button>
+
+     
+  {words.map((word,idx)=>(
+  <div key={idx}>{word}</div>
+   ))}
+
+    </>
   );
 };
 
