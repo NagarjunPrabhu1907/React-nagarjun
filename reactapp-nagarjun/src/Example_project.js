3fa6585fe51a084a9c834c911f0f19edebbf9756
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-12-05 15:05:20 +0530

    Update Example_project.js

--- reactapp-nagarjun/src/Example_project.js
+++ reactapp-nagarjun/src/Example_project.js
@@ -2,13 +2,20 @@ import React, { useState,useEffect,useRef } from "react";
 import {useNavigate} from 'react-router-dom';
 import NextPage from "./NextPage";
 
+
 export default function Example_project() {
-    const [NameValue, setName] = useState(' ');
-    const [emailValue, setEmail] = useState(' ');
-    const [CompanyValue, setCompany] = useState(' ');
-    const [locationValue, setLocation] = useState(' ');
-   
+    const [NameValue, setName] = useState();
+    const [emailValue, setEmail] = useState();
+    const [CompanyValue, setCompany] = useState();
+    const [locationValue, setLocation] = useState();
     const navigate = useNavigate();
+
+
+    const [error1, setError1] = useState(null);
+    const [error2, setError2] = useState(null);
+    const [error3, setError3] = useState(null);
+    const [error4, setError4] = useState(null);
+    
 
     function Reset() {
         setName("")
@@ -40,7 +47,7 @@ export default function Example_project() {
         localStorage.setItem("location", locationValue)
         window.alert('Data saved successfully');
     }
-
+  
     const containerStyle = {
         display: 'flex',
         // flexDirection: 'column',
@@ -51,11 +58,53 @@ export default function Example_project() {
     const labelStyle = {
         marginRight: '8px',
       };
-    
+      const handleName = e=>{
+        if (!validateName(e.target.value)){
+            setError1("name invalid")
+            }else
+            {
+                setError1(null);
+            }
+            setName(e.target.value);
+      }
+      const handleEmail = e=>{
+        if (!validateEmail(e.target.value)){
+            setError2("email invalid")
+            }else
+            {
+                setError2(null);
+            }
+            setEmail(e.target.value);
+      }
+      const handleCompanyName = e=>{
+        if (!validateCompany(e.target.value)){
+            setError3("company name invalid")
+            }else
+            {
+                setError3(null);
+            }
+            setCompany(e.target.value);
+      }
+      const handleLocation = e=>{
+        if (!validateLocation(e.target.value)){
+            setError4("location invalid")
+            }else
+            {
+                setError4(null);
+            }
+            setLocation(e.target.value);
+      }
       
+
+  const validateName = () => /^[A-Za-z\s]*$/.test(NameValue);
+  const validateEmail = () => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
+ const validateCompany = () => /^[A-Za-z\s]*$/.test(CompanyValue);
+  const validateLocation = () => /^[A-Za-z\s]*$/.test(locationValue);
+
+
     return (
         <div>
-            <h1> new project</h1>
+            <h1> Application</h1>
             
             <div style={containerStyle}>
                 <label style={labelStyle}>  Name : </label>
@@ -63,11 +112,13 @@ export default function Example_project() {
                     type="text"
                     value={NameValue}
                     placeholder="Name"
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={handleName}
                     style={inputStyle}
                     ref={ref}
+                     
                 />
             </div>
+            {error1 && <h2 style={{color: 'red'}}>{error1}</h2>}
             <br />        
             <div style={containerStyle}>
             <label style={labelStyle}> Email : </label>
@@ -75,11 +126,13 @@ export default function Example_project() {
                     type="text"
                     value={emailValue}
                     placeholder="Email ID"
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={handleEmail}
                     style={inputStyle}
                     
+
                 />
             </div>
+            {error2 && <h2 style={{color: 'red'}}>{error2}</h2>}
             <br />
             
             <div style={containerStyle}>
@@ -88,10 +141,12 @@ export default function Example_project() {
                     type="text"
                     value={CompanyValue}
                     placeholder="Company Name"
-                    onChange={(e) => setCompany(e.target.value)}
+                    onChange={handleCompanyName}
                     style={inputStyle}
+                 
                 />
             </div>
+            {error3 && <h2 style={{color: 'red'}}>{error3}</h2>}
             <br />
             
             <div style={containerStyle}>
@@ -100,21 +155,19 @@ export default function Example_project() {
                     type="text"
                     value={locationValue}
                     placeholder="Location"
-                    onChange={(e) => setLocation(e.target.value)}
+                    onChange={handleLocation}
                     style={inputStyle}
-                />
+                        
+                     />
                 <br />
             </div>
+            {error4 && <h2 style={{color: 'red'}}>{error4}</h2>}
             <br/>
             <br/>
             <br/>
-            <button onClick={saveToLocalStorage}>submit</button>
-            {" "}
-            <button onClick={Reset} > clear </button>
-            {" "}
-            <button onClick={()=> navigate('/next-page')} > next page </button>
-
-            
+            <button style={{marginLeft:"10px"}} onClick={saveToLocalStorage}>submit</button>
+            <button style={{marginLeft:"20px"}} onClick={Reset} > clear </button>
+            <button style={{marginLeft:"25px"}} onClick={()=> navigate('/next-page')} disabled={!NameValue || !emailValue|| !CompanyValue || !locationValue} > next page </button>
 
         </div>
     )
