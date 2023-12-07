9504495412a243ef3b455aafce21630fd94ba690
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-12-07 17:03:00 +0530

    dataArray[]
    data are passed into array

--- reactapp-nagarjun/src/Example_project.js
+++ reactapp-nagarjun/src/Example_project.js
@@ -4,11 +4,12 @@ import NextPage from "./NextPage";
 
 
 export default function Example_project() {
-    const [NameValue, setName] = useState('');
+    const [nameValue, setName] = useState('');
     const [emailValue, setEmail] = useState('');
-    const [CompanyValue, setCompany] = useState('');
+    const [companyValue, setCompany] = useState('');
     const [locationValue, setLocation] = useState('');
     const navigate = useNavigate();
+    const [dataEntries,setdataEntries]=useState([]);
 
 
     function Reset() {
@@ -17,36 +18,51 @@ export default function Example_project() {
         setCompany("")
         setLocation("")
     }
-    // useEffect (()=> {
-    //     const storedName = localStorage.getItem("name");
-    //     const storedEmail = localStorage.getItem("email");
-    //     const storedCompany = localStorage.getItem("company name");
-    //     const storedLocation = localStorage.getItem("location");
-
+     useEffect (()=> {
+        const storedName = localStorage.getItem("name");
+        const storedEmail = localStorage.getItem("email");
+        const storedCompany = localStorage.getItem("company name");
+        const storedLocation = localStorage.getItem("location");
+      
+        // if (nameValue !== storedName || companyValue!== storedCompany || emailValue!==storedEmail || locationValue!==storedLocation){
+        //     window.alert("data changed in local storage")
+        // }
 
     //    setName(storedName); 
     //     setEmail(storedEmail);
     //     setCompany(storedCompany);
     //     setLocation(storedLocation);
-    // },[])
+     },[])
 
     const ref = useRef();
     useEffect(() => {
         ref.current.focus();
     }, []);
+
+//     useEffect(() => {
+// window.alert("default name")
+//     }, [nameValue]);
     function saveToLocalStorage() {
-        localStorage.setItem("name", NameValue)
-        localStorage.setItem("email", emailValue)
-        localStorage.setItem("company name", CompanyValue)
-        localStorage.setItem("location", locationValue)
-        navigate('/next-page', {
-            state: {
-                name: NameValue,
-                email: emailValue,
-                companyName: CompanyValue,
-                location1: locationValue,
-            }
-        });
+       // localStorage.setItem("name", nameValue)
+       // localStorage.setItem("email", emailValue)
+       // localStorage.setItem("company name", companyValue)
+        //localStorage.setItem("location", locationValue)
+        // navigate('/next-page', {
+        //     state: {
+        //         name: nameValue,
+        //         email: emailValue,
+        //         companyName: companyValue,
+        //         location1: locationValue,
+        //     }
+        // });
+    const dataBox = 
+    {
+        name : nameValue,
+        email: emailValue,
+        companyname : companyValue,
+        location1 : locationValue
+    };
+    setdataEntries([...dataEntries,dataBox]);
 
         window.alert('Data saved successfully');
     }
@@ -71,7 +87,6 @@ export default function Example_project() {
             setName('');
         }
     }
-
     const handleEmail = e => {
         const c = e.target.value;
         const d = /^[A-Za-z0-9@!#$%^&.]+$/.test(c);
@@ -106,6 +121,10 @@ export default function Example_project() {
         }
     }
 
+// const setDefaultName = () =>{
+//     setName("default");
+// }
+  
 
     return (
         <div>
@@ -115,7 +134,7 @@ export default function Example_project() {
                 <label style={labelStyle}>  Name : </label>
                 <input
                     type="text"
-                    value={NameValue}
+                    value={nameValue}
                     placeholder="Name"
                     onChange={handleName}
                     style={inputStyle}
@@ -142,7 +161,7 @@ export default function Example_project() {
                 <label style={labelStyle}> Company : </label>
                 <input
                     type="text"
-                    value={CompanyValue}
+                    value={companyValue}
                     placeholder="Company Name"
                     onChange={handleCompanyName}
                     style={inputStyle}
@@ -169,9 +188,30 @@ export default function Example_project() {
             <br />
             <br />
 
-            <button style={{ marginLeft: "20px" }} onClick={Reset} > clear </button>
-            <button style={{ marginLeft: "25px" }} onClick={saveToLocalStorage} disabled = {!NameValue||!emailValue||!CompanyValue||!locationValue||" "}> next page </button>
+            <button style={{ marginLeft: "20px" }} onClick={Reset} > CLEAR </button>
+            <button style={{ marginLeft: "25px" }} onClick={saveToLocalStorage} disabled = {!nameValue.trim()||!emailValue.trim()||!companyValue.trim()||!locationValue.trim()}> SUBMIT </button>
+            {/* <button style={{ marginLeft: "20px" }} onClick={setDefaultName} > setDefault Name </button> */}
+            <br />
+            <br />
 
+            <div>
+           <h3>Data entered :</h3>
+           <ul>
+            {dataEntries.map((data,index)=>(
+                <li> key={index}
+                <br/>
+                name :{data.name},Email: {data.email}, Company: {data.companyname}, Location: {data.location1}
+                </li>
+            )
+            
+            )}
+
+
+
+           </ul>
+
+
+            </div>
         </div>
     )
 }
